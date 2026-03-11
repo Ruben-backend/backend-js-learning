5d433f2ed1b18b29aa37bdc44229051ee749c4a9
Ruben <ruben.ethicalhacker@gmail.com>
2026-03-11 09:15:06 -0600

    Ejercicos faltentes de la pagian web

--- dia2.js
+++ dia2.js
@@ -58,12 +58,83 @@ if (usuario === "admin" && password === "1234")
     if ("0") {
       console.log( 'Hello' );
     }
-    // Diria que el alert(en mi caso el console) no se monstraira pues por lo leido anteriromente en la pagina al cero se le conoce como “falsy” por lo que no deberia imprimir el hola
-    // Solucion
+    // Diria que el alert(en mi caso el console) no se monstraria pues por lo leido anteriromente en la pagina al cero se le conoce como “falsy” por lo que no deberia imprimir el hola
+// --  Solucion --
 
 // Sí lo hará.
 // Cualquier string excepto uno vacío (y "0" que no es vacío) se convierte en true en un contexto lógico.
-// Nota personal: Ver los prqueños detalles .
+// Nota personal: Ver los prqueños detalles 
 
+//2. Muestra el signo/importancia: 2
+// Usando el constructor if..else, escribe un código que obtenga a través de un prompt un número y entonces muestre en un alert:
+// 1, si el valor es mayor que cero,
+// -1, si es menor que cero,
+// 0, si es igual a cero.
+// En la tarea asumimos que siempre el usuario introduce un número.
 
+let value = 10; // Remplaze el promt por un valor ya determinado por que lo estoy haciendo desde nodejs
 
+if (value > 0){
+    console.log(1);
+} else if (value < 0){
+    console.log(-1);
+} else {
+    console.log(0)
+}
+
+// -- SOlUCION --
+// let value = prompt('Escribe un número', 0);
+
+// if (value > 0) {
+//   alert( 1 );
+// } else if (value < 0) {
+//   alert( -1 );
+// } else {
+//   alert( 0 );}
+
+// Nota:Al parecer estube correcto en todo asique que bien.
+
+//3. Reescribe el 'if' como '?'/importancia: 5
+// Reescriba esta condición if usando el operador ternario '?':
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Debajo';
+// } else {
+//   result = 'Encima';}
+
+// Mi solucion:
+
+//let result;
+// (a + b < 4) ? result = 'Debajo': result = 'Encima'; // Mi RESPUESTA PERO LA COMENTO PARA QUE NO MARQUE ERROR EN LA CONSOLA
+
+// -- SOLUCION --
+    // let result = (a + b < 4) ? 'Debajo' : 'Encima';
+
+//Nota:Cabe aclarar que me salio algo similiar
+
+// 4. Reescriba el 'if..else' con '?'/ importancia: 5
+// Reescriba el if..else utilizando operadores ternarios múltiples'?'.
+// Para mejor legibilidad, se recomienda dividirlo en múltiples lineas de código./
+// let message;
+
+// if (login == 'Empleado') {
+//   message = 'Hola';
+// } else if (login == 'Director') {
+//   message = 'Felicidades';
+// } else if (login == '') {
+//   message = 'Sin sesión';
+// } else {
+//   message = '';}
+
+// Mi solucion : (la comento para que no marque error en la consola)
+// let message = (login == 'Empleado') ? 'Hola':
+// (login == 'Director')? 'Felicidades':
+// (login == '')? 'Sin sesión':
+// ''; - Esto me falto 
+
+// -- SOLUCION --
+// let message = (login == 'Empleado') ? 'Hola' :
+  // (login == 'Director') ? 'Felicidades' :
+  // login == '') ? 'Sin sesión' :
+  // '';
